2c762befe470bc61df595bffa3c931c1400605d0
Ciurea Robert-Mihai <ciurea.robert01@gmail.com>
2024-12-10 10:32:28 +0200

    landing page done

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -10,6 +10,27 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+    const cardsContainer = document.querySelector(".destination-cards");
+
+    if (prevButton != null && nextButton != null && cardsContainer != null){
+      // Scroll left when the "prev" button is clicked
+      prevButton.addEventListener("click", () => {
+        cardsContainer.scrollBy({
+          left: -300, // Scroll by 300px to the left
+          behavior: "smooth",
+        });
+      });
+
+      // Scroll right when the "next" button is clicked
+      nextButton.addEventListener("click", () => {
+        cardsContainer.scrollBy({
+          left: 300, // Scroll by 300px to the right
+          behavior: "smooth",
+        });
+      });
+    }
   }
 
 }
