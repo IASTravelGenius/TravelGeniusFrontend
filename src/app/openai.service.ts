6a42ddde1bf6b6a95934fb42f6dbcdb1d80b80ac
Ciurea Robert-Mihai <ciurea.robert01@gmail.com>
2025-01-07 17:59:17 +0200

    removed openai

--- src/app/openai.service.ts
+++ src/app/openai.service.ts
@@ -9,7 +9,7 @@ export class OpenaiService {
   private apiUrl = "https://api.openai.com/v1/chat/completions";
   private projectId = "	proj_Ve1rwcoz7nN7wFr4mcUr1i8H";
   private organization = "org-HFS2No9ZB2fOZ002OlnPK2Np";
-  private apiKey = import.meta.env.NG_APP_OPENAI_KEY;
+  private apiKey = '';
 
   private headerText: string = 'Provide 25 cities and their coordinates based on the sentence provided. Do not stop at one country, provide to multiple countries based on the information provided based on what the user wants to visit in a holiday, national or international. Keep it to one country only if the user specifies that country or something like that:\n'
   private footerText: string = '\nPlease short answear, just the information provided because I will just make some string operation to extract the city and the coordinates, write them as a json to be easy to extract'
--- src/app/plan-trip/plan-trip.component.ts
+++ src/app/plan-trip/plan-trip.component.ts
@@ -19,20 +19,20 @@ export class PlanTripComponent implements OnInit {
     if (this.searchContent === "") {
       alert("Please provide search parameters");
     } else {
-      this.openaiService.generateText(this.searchContent).subscribe({
-        next: (response) => {
-          const content = response["choices"][0]["message"]["content"];
-          const lines = content.split('\n');
-          lines.shift()
-          lines.pop()
-          const cleanedContent = lines.join('\n');
-          console.log(cleanedContent);
-          console.log(JSON.parse(cleanedContent));
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
+      // this.openaiService.generateText(this.searchContent).subscribe({
+      //   next: (response) => {
+      //     const content = response["choices"][0]["message"]["content"];
+      //     const lines = content.split('\n');
+      //     lines.shift()
+      //     lines.pop()
+      //     const cleanedContent = lines.join('\n');
+      //     console.log(cleanedContent);
+      //     console.log(JSON.parse(cleanedContent));
+      //   },
+      //   error: (err) => {
+      //     console.log(err);
+      //   },
+      // });
        setTimeout(() => {
          this.router.navigate(["/destination-suggestion"]);
        }, 1000);
